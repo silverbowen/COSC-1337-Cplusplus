13fb45bf47e34752367079b9c2cda1f0c11de47a
silverbowen <silver.quickness@gmail.com>
2015-06-01 13:46:05 -0500

    Style tidied up

--- assignment01.cpp
+++ assignment01.cpp
@@ -1,5 +1,6 @@
 /* Program by Silver Bowen
    COSC 1337 - summer 2015
+   Status - Complete
 
    This program asks the user for two
    integers, then performs math operations
@@ -13,44 +14,46 @@ using namespace std;
 
 int main()
 {
-    // ask user for input.
+    // Ask user for input.
     cout << "Enter two integers, separated by a space: ";
 
-    // variables declared.
-    int num1, num2;
+    // Variables declared.
+    int num1, // stores 1st user input number
+        num2; // stores 2nd user input number
 
-    // variables initialized via cin.
+    // Variables initialized via cin.
     cin >> num1 >> num2;
 
-    // spacer for tidiness.
+    // Spacer for tidiness.
     cout << endl;
 
-    /* print various manipulations,
-       formatting exactly as per example */
+    /* The rest of the program prints the
+       various manipulations. I've tried to keep
+       the formatting exactly as per the example. */
 
-    // Sum simple adds the numbers.
+    // Sum - simply adds the numbers.
     cout << left << setw(10) << "Sum";
     cout <<"is " << (num1 + num2) << endl;
     cout << endl;
 
-    // Product simply multiplies them.
+    // Product - simply multiplies them.
     cout << left << setw(10) << "Product";
     cout <<"is " << (num1 * num2) << endl;
     cout << endl;
 
-    // Average adds the numbers, then divides by two.
+    // Average - adds the numbers, then divides by two.
     // These are ints == any decimal places are truncated.
     cout << left << setw(10) << "Average";
     cout <<"is " << ((num1 + num2) / 2) << endl;
     cout << endl;
 
-    // Quotient divides the 2nd number by the 1st.
+    // Quotient - divides the 2nd number by the 1st.
     // These are ints == any decimal places are truncated.
     cout << left << setw(10) << "Quotient";
     cout <<"is " << (num1 / num2) << endl;
     cout << endl;
 
-    // Modulus gets the remainder of the 1st number
+    // Modulus - gets the remainder of the 1st number
     // divided by the 2nd number.
     cout << left << setw(10) << "Modulus";
     cout <<"is " << (num1 % num2) << endl;
